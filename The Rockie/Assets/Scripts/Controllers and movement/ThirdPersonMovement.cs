45b721243917794f916224ca2970ea76e3125802
unki113d <unkilled135@gmail.com>
2023-12-19 20:10:04 +0200

    Доделано перемещение через transform
    - Переписал код перемещения
    - Отключил ротацию по осям X и Z

--- The Rockie/Assets/Scripts/Controllers and movement/ThirdPersonMovement.cs
+++ The Rockie/Assets/Scripts/Controllers and movement/ThirdPersonMovement.cs
@@ -7,15 +7,15 @@ public class ThirdPersonMovement : MonoBehaviour
     [HideInInspector] public Vector3 direction;
 
     [SerializeField] private float speed, rotationSpeed, jumpForce;
-    [SerializeField] private Transform target;
-    [SerializeField] private Transform groundPoint;
+/*    [SerializeField] private Transform target;
+    [SerializeField] private Transform groundPoint;*/
 
     static public GameObject playerObject;
     private float distToGround;
-    private float _moveX, _moveY;
+    public float _moveX, _moveY;
 
     private Rigidbody _rb;
-    private Animator _anim;
+    //private Animator _anim;
     public Collider _playerCollider;
     void Start()
     {
@@ -29,14 +29,16 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetDirection();
-    }
-
-    void GetDirection()
-    {
         _moveX = Input.GetAxis("Horizontal");
         _moveY = Input.GetAxis("Vertical");
-        direction = transform.forward * _moveY + transform.right * _moveX;
+        PhysicalMove();
+    }
+
+    void PhysicalMove()
+    {
+        Vector3 physicsMove = transform.forward * _moveY * speed + transform.right * _moveX * speed;
+        physicsMove.y = _rb.velocity.y;
+        _rb.velocity = physicsMove;
     }
 
     bool IsGrounded()
